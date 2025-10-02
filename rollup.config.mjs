add7ebfae5a55d6434ef42f41c8650329acf8ff8
FearDread <ghaptonstall@gmail.com>
2025-10-02 13:18:01 -0400

    --update rollup to include this context

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,27 +2,29 @@ import { terser } from 'rollup-plugin-terser';
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/jquery.fear.bundle.js',
+  input: 'src/gui/core/index.js',
   output: [
-      {
+    {
       file: 'dist/jquery.fear.js', // Non-minimized (development)
       format: 'esm'
-  },
-  {
-    file: 'dist/jquery.fear.min.js',
-    format: 'umd',
-    name: 'FEAR',
-    globals: {
-      jquery: '$'
-    }
-  },
-],
+    },
+    {
+      file: 'dist/jquery.fear.min.js',
+      format: 'umd',
+      name: 'FEAR',
+      globals: {
+        jquery: '$'
+      },
+      plugins: [
+        babel({
+          babelHelpers: 'bundled',
+          presets: ['@babel/preset-env']
+        }),
+        terser()
+      ]
+    },
+  ],
   external: ['jquery'],
-  plugins: [
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/preset-env']
-    }),
-    terser()
-  ]
+  context: 'this'
+
 };
